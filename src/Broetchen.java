155b79c2739d35cfc3ac9b6bf3a3efd74510d2f5
ThiloWittmer <thilo.wittmer@gmail.com>
2022-05-20 16:36:36 +0200

    small changes

--- src/Broetchen.java
+++ src/Broetchen.java
@@ -10,30 +10,21 @@ public class Broetchen extends Zutaten {
 		switch(hoehe) {
 		case 27: 
 			broetchenName = "Hamburger (Standard)";
-			backzeit = 1.5;
+			backzeit = (float) 1.5;
 		case 28: 
 			broetchenName = "Hamburger Sesam";
-			backzeit = 1.5;
+			backzeit = (float) 1.5;
 		case 34: 
 			broetchenName = "Vegan-Br�tchen";
-<<<<<<< HEAD
-			backzeit = 4.0;
-=======
-			backzeit = 240;
->>>>>>> e1248bf9e1fdd211f566798eb3c040800ceb17b9
+			backzeit = (float) 4.0;
 		case 41: 
 			broetchenName = "Ciabatta";
-			backzeit = 5.5;
+			backzeit = (float) 5.5;
 		}
 	}
 	
-<<<<<<< HEAD
 	public float zubereiten() {
 		System.out.println(broetchenName + " " + backzeit + " Minuten rösten und schneiden.");
-=======
-	public int zubereiten() {
-		System.out.println(broetchenName + " " + (float) backzeit/60 + " Minuten roesten und aufschneiden");
->>>>>>> e1248bf9e1fdd211f566798eb3c040800ceb17b9
 		return backzeit;
 	}
 
