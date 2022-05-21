cdc165ddfa5cea622ec9dbf2115e95fb5f26cc31
ThiloWittmer <thilo.wittmer@gmail.com>
2022-05-21 18:33:36 +0200

    created Burger + anweisungen

--- src/Burger.java
+++ src/Burger.java
@@ -1,8 +1,27 @@
+/***
+ * Data:
+•	String: name
+•	Object [9]: Zutaten
+•	Int: anzahlZutaten 
+
+Methoden:
+•	Float: berechnePreis()
+•	Float: berechneHoehe()
+•	Boolean: istVegan()
+•	Boolean: istVegetarisch()
+•	Boolean: istMitFleisch()
+•	String: geschmack() [abhängig von Sauce]
+•	Boolean: klassisch()
+•	Int: zeitBerechnen()
+•	Boolean: zutatHinzufuegen(int index) // boolean: existiert Brötchen?
+•	String: zutatenString() (Alle Zutaten in ein String)
+
+ */
 
 public class Burger {
-	
-	//Die einzel ausgewählten Zutaten des Burgers in einem Array verwalten
-	
-	//Ein Burger darf nicht mehr als 9 Zutaten haben (inlk. des Brötchens)
+	private String name;
+	private Zutaten[] zutaten = new Zutaten[9];
+	private int anzahlZutaten;
 
+	
 }
