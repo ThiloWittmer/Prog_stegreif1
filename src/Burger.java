725c73ab24d983820f3ffbfd6b1e1b27184a6101
asyx23 <info@aaron-stier.de>
2022-05-21 18:32:24 +0200

    Klasse gelöscht

--- src/Burger.java
+++ src/Burger.java
@@ -1,7 +1,9 @@
 
 public class Burger {
 	
-	//Die einzel ausgewählten Zutaten des Burgers in einem Array verwalten
+	
+	
+	//Die einzel ausgewählten Zutaten (Objekte) des Burgers in einem Array verwalten
 	
 	//Ein Burger darf nicht mehr als 9 Zutaten haben (inlk. des Brötchens)
 
