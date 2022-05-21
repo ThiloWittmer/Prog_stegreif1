3a8ed4025acbd33d637fbb926f1c7397729464e2
asyx23 <info@aaron-stier.de>
2022-05-21 16:44:54 +0200

    Kommentare wieder rausgenommen

--- src/Control.java
+++ src/Control.java
@@ -2,15 +2,7 @@
 public class Control {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		//test kommentar 
-		// tst 
-		
-		int i = 0;
-		//fest sdijfgsdifüasodfüasodfsdaf
-		//test
+	
 	}
 
 }
