3be68852f1e81ff6f06854f8cf93a48cfcf21b82
asyx23 <info@aaron-stier.de>
2022-05-21 17:30:47 +0200

    Bestellliste

--- src/Bestellliste.java
+++ src/Bestellliste.java
@@ -3,4 +3,6 @@ public class Bestellliste {
 	//sehen Sie die Verwaltung der zusammengestellten Burger als Bestellliste
 	
 	//ein Array/ eigene Klasse in dem alle kreierten Burger abgelegt werden
+	
+	//max. 10 Burger bestellen
 }
