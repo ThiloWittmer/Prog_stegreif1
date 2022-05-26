db2a8c1b62801f846ed3689a4b857b82f46a2865
muhanadcr7 <muhanadkhatib998@gmail.com>
2022-05-26 17:35:40 +0200

    Zutaten und ZutatenManger doku

--- src/Zutaten.java
+++ src/Zutaten.java
@@ -1,3 +1,12 @@
+/***
+ * @class Zutaten - Werte Definiton der Zutaten der OberKlasse
+ * @parameter nummer - jede Zutat hat eine Nummer  
+ * @parameter name - jede Zutat hat einen Name 
+ * @parameter preis - jede Zutat hat einen Preis
+ * @parameter klassisch - die Zutat kann klassisch sein
+ * @parameter vegan- die Zutat kann vegan sein
+ * @parameter vegetarisch - die Zutat kann vegetarisch sein 
+ */
 
 public abstract class Zutaten {
 	protected int nummer;
@@ -7,6 +16,11 @@ public abstract class Zutaten {
 	protected boolean vegan;
 	protected boolean vegetarisch;
 	
+	/***
+	 *die Methode zubereiten - Ausgabe jeder Zutat, wie sie zubereitet wird
+	 * @return gibt wie viele Sekunden die Zubereitung gedauert hat zurück
+	 * die Methode berechneHoehe - berechnung der Hoehe vom Bürger
+	 */
 	public abstract int zubereiten();
 	public abstract float berechneHoehe();
 	
